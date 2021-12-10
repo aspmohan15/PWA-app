59e461fe0769e5b74ffe907665ca4e3a74830823
mohan <mohanp.18cse@kongu.edu>
2021-12-10 17:39:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Ya this is --PWA-- app you can install it</h1>
+    </div>
+  );
+}
+
+export default App;
